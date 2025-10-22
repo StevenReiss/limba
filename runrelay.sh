7676b17b40de1d81b33eeb291f871444b084e563
Steven Reiss <spr@cs.brown.edu>
2025-10-22 07:38:46 -0400

    Fix run relay on fred4.

--- runrelay.sh
+++ runrelay.sh
@@ -5,7 +5,7 @@ pushd $SRD > /dev/null
 SRD1=`pwd`
 popd > /dev/null
 
-java -jar $SRD1/limbarelay.jar -h llmserver.cs.brown.edu
+java -jar $SRD1/limbarelay.jar -h llmserver.cs.brown.edu -p 11435
 
 
 
